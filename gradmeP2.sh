7eb91a23bcbd915754badaf3b55a0a185c8a6f6f
macrespo42 <maxime.crespopro@gmail.com>
2019-10-14 21:49:56 +0200

    script test

---
+++ gradmeP2.sh
@@ -0,0 +1,5 @@
+make
+gcc -Wall -Wextra -Werror mainP2.c -L. libft.a
+./a.out
+rm a.out
+make fclean
---
+++ mainP2.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mainP2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/14 19:59:44 by macrespo          #+#    #+#             */
+/*   Updated: 2019/10/14 21:49:36 by macrespo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "stdio.h"
+
+void	substr()
+{
+	for (int i = 0; i < 5; i++)
+		printf("\n");
+	printf("TESTING FT_SUBSTR :\n");
+	printf("[With NULL string : %s] --- [expect : NULL]\n", ft_substr(NULL, 10, 5));
+	printf("[With empty string : %s] --- [expect : empty string]\n", ft_substr("", 10, 5));
+	printf("[With string and 0 len : %s] --- [expect : empty string]\n", ft_substr("Hello world", 5, 0));
+	printf("[Normal case : %s] --- [expect : Hello]\n", ft_substr("Hello world", 0, 5));
+	printf("[With string and 0 len : %s] --- [expect : empty string]\n", ft_substr("Hello world", 400, 5));
+	printf("[With string and 0 len : %s] --- [expect : world]\n", ft_substr("Hello world", 6, 400));
+	printf("[With string and 0 len : %s] --- [expect : empty string]\n", ft_substr("Hello world", 400, 400));
+	for (int i = 0; i < 5; i++)
+		printf("\n");
+}
+
+void	strjoin()
+{
+	for (int i = 0; i < 5; i++)
+		printf("\n");
+	printf("TESTING FT_STRJOIN : \n");
+	printf("[With normal params : %s] --- [expect : HelloWorld]\n", ft_strjoin("Hello", "World"));
+	printf("[With first param NULL : %s] --- [expect : NULL]\n", ft_strjoin(NULL, "World"));
+	printf("[With second param NULL : %s] --- [expect : NULL]\n", ft_strjoin("Hello", NULL));
+	for (int i = 0; i < 5; i++)
+		printf("\n");
+}
+
+void	strtim()
+{
+	for (int i = 0; i < 5; i++)
+		printf("\n");
+	printf("TESTING FT_STRTRIM : \n");
+	printf("[String with only set : %s] --- [expect : empty string]\n", ft_strtrim("     a ", " a"));
+	printf("[Normal arg : %s] --- [expect : Hello World]\n", ft_strtrim("   ___Hello World   __", " _"));
+	printf("[With NULL string : %s] --- [expect : NULL]\n", ft_strtrim(NULL, " "));
+	printf("[With NULL set : %s] --- [expect : NULL]\n", ft_strtrim("Hey you", NULL));
+	printf("[With empty set : %s] --- [expect : __Hey you__]\n", ft_strtrim("__Hey you__", ""));
+	printf("[With empty string : %s] --- [expect : empty string]\n", ft_strtrim("", " "));
+	for (int i = 0; i < 5; i++)
+		printf("\n");
+}
+
+int		main(void)
+{
+	substr();
+	strjoin();
+	strtim();
+	return (0);
+}

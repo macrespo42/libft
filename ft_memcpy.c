fb961406b357cd03c7eb563c0e5f668a6f73908b
Maxime CRESPO <macrespo@e1r6p1.42.fr>
2019-10-18 09:41:20 +0200

    protect memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 19:48:41 by macrespo          #+#    #+#             */
-/*   Updated: 2019/10/07 20:19:42 by macrespo         ###   ########.fr       */
+/*   Updated: 2019/10/18 09:40:49 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void		*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char		*dst_cp;
 	unsigned char		*src_cp;
 
+	if (!dst || !src)
+		return (NULL);
 	dst_cp = (unsigned char*)dst;
 	src_cp = (unsigned char*)src;
 	i = 0;

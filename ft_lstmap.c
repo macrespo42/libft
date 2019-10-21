2f8f3d2ce96dd6517cb24481c7253de0f9474d3c
Maxime CRESPO <macrespo@e1r2p6.42.fr>
2019-10-21 09:05:15 +0200

    norm lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,38 +6,37 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/11 09:26:53 by macrespo          #+#    #+#             */
-/*   Updated: 2019/10/18 13:45:33 by macrespo         ###   ########.fr       */
+/*   Updated: 2019/10/21 09:03:12 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list		*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *))
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list		*new_l;
 	t_list		*tmp;
-	t_list		*prev;
-	t_list		*start;
+	t_list		*new;
+	t_list		*out;
 
-	if (!lst || !f)
+	if (!lst && !f)
 		return (NULL);
 	tmp = lst;
-	start = NULL;
-	prev = NULL;
-	while (tmp)
+	if (!(out = ft_lstnew(f(tmp->content))))
+		return (NULL);
+	new = out;
+	while (tmp->next)
 	{
-		if (!(new_l = (t_list*)malloc(sizeof(t_list))))
-		{
-			del(lst);
-			return (NULL);
-		}
-		if (prev)
-			prev->next = new_l;
-		prev = new_l;
-		new_l->content = f(tmp->content);
-		if (start == NULL)
-			start = new_l;
 		tmp = tmp->next;
+		if (!(out->next = ft_lstnew(f(tmp->content))))
+			return (new);
+		del ? del(out->next->content) : 0;
+		if (!out->next->content)
+		{
+			free(out->next);
+			out->next = NULL;
+		}
+		else
+			out = out->next;
 	}
-	return (start);
+	return (new);
 }
